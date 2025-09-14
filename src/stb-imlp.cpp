cacd91fb637895428c45618f271e7d978c928138
Lotar122 <dajlizaka9@gmail.com>
2025-09-14 09:48:19 +0200

    Trying fix cross-platform compatibility v3

--- src/stb-imlp.cpp
+++ src/stb-imlp.cpp
@@ -1,2 +1,6 @@
 #define STB_IMAGE_IMPLEMENTATION
+#ifdef WINDOWS
 #include <stb_image.h>
+#else
+#include <stb/stb_image.h>
+#endif
---
+++ src/stb_image.h
@@ -0,0 +1,6 @@
+#pragma once
+#ifdef WINDOWS
+#include <stb_image.h>
+#else
+#include <stb/stb_image.h>
+#endif
